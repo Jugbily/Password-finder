25951087fcc3788131bfd9a684096b299c7b8f00
jugbily <jungwonqv@naver.com>
2024-04-20 23:21:35 +0900

    Spacing, cleaner code.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static Key[] userArray = new Key[200];
     public static int size = 0;
+
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         while (true) {
